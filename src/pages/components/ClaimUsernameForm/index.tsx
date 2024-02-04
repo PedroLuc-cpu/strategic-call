2af50418cdf380ccf83b7ed12d17838cabf36dc3
pedroluc-cpu <pedrolucas_gta2015@hotmail.com>
2024-02-03 21:24:23 -0300

    fix: Criado a validação com Zod

--- src/pages/components/ClaimUsernameForm/index.tsx
+++ src/pages/components/ClaimUsernameForm/index.tsx
@@ -1,36 +1,59 @@
-import { Button, TextInput } from '@ignite-ui/react'
-import { Form } from './styles'
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { Form, FormAnnotation } from './styles'
 import { ArrowRight } from '@phosphor-icons/react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
 const ClaimUsernameSchema = z.object({
-  username: z.string(),
+  username: z
+    .string()
+    .min(3, { message: 'usuario dever conter 3 caracteres.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuario pode ter apenas caracteres e hifens.',
+    })
+    .transform((username) => username.toLowerCase()),
 })
 
 type ClaimUsernameFormData = z.infer<typeof ClaimUsernameSchema>
 
 export const ClaimUsernameForm = () => {
-  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(ClaimUsernameSchema),
+  })
 
   async function handleClaimUsername(data: ClaimUsernameFormData) {
     console.log(data)
   }
 
   return (
-    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
-      <TextInput
-        size={'sm'}
-        prefix="strategic.com/"
-        placeholder="your username"
-        crossOrigin="anonymous"
-        autoComplete="off"
-        {...register('username')}
-      />
-      <Button size={'sm'} type="submit">
-        Reservar
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size={'sm'}
+          prefix="strategic.com/"
+          placeholder="your username"
+          crossOrigin="anonymous"
+          autoComplete="off"
+          {...register('username')}
+        />
+        <Button size={'sm'} type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+
+      <FormAnnotation>
+        <Text size={'sm'}>
+          {errors.username
+            ? errors.username.message
+            : 'Please enter your username'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
