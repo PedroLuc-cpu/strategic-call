81cfc4e5c9221320a403d8571b097dc6b9fbf8ac
pedroluc-cpu <pedrolucas_gta2015@hotmail.com>
2024-02-06 23:21:29 -0300

    fix: Conexão com calendário - Criando o usuário no adapter e persistencia dos dados do usuário do google

---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import NextAuth from 'next-auth'
+
+declare module 'next-auth' {
+  export interface User {
+    id: string
+    username: string
+    name: string
+    email: string
+    avatar_url: string
+  }
+}
