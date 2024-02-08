615788c8a5b691b75a3d99b534abc5a558a65719
pedroluc-cpu <pedrolucas_gta2015@hotmail.com>
2024-02-07 22:51:23 -0300

    fix:  Configurando disponibilidade - Formulário de disponibilidade

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -15,8 +15,44 @@ import {
   IntervalItem,
 } from './styles'
 import { ArrowRight } from '@phosphor-icons/react'
+import { useFieldArray, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { getWeekDays } from '@/utils/get-week-day'
+
+const timeIntervalTimeSchema = z.object({})
 
 export default function TimeIntervals() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    defaultValues: {
+      intervals: [
+        { weekday: 0, enabled: false, starTime: '08:00', endTime: '18:00' },
+        { weekday: 1, enabled: true, starTime: '08:00', endTime: '18:00' },
+        { weekday: 2, enabled: true, starTime: '08:00', endTime: '18:00' },
+        { weekday: 3, enabled: true, starTime: '08:00', endTime: '18:00' },
+        { weekday: 4, enabled: true, starTime: '08:00', endTime: '18:00' },
+        { weekday: 5, enabled: true, starTime: '08:00', endTime: '18:00' },
+        { weekday: 6, enabled: false, starTime: '08:00', endTime: '18:00' },
+      ],
+    },
+  })
+
+  const weekDays = getWeekDays()
+
+  // permite interar ou manipular campos de um array
+  const { fields } = useFieldArray({
+    control,
+    name: 'intervals',
+  })
+
+  async function handleSetTimeIntervals() {
+    return console.log()
+  }
+
   return (
     <Container>
       <Header>
@@ -27,48 +63,34 @@ export default function TimeIntervals() {
         </Text>
         <MultiStep size={4} currentStep={3} />
       </Header>
-      <IntervalBox as="form">
+      <IntervalBox as="form" onSubmit={handleSetTimeIntervals}>
         <IntervalContainer>
-          <IntervalItem>
-            <IntervalDay>
-              <Checkbox />
-              <Text>Segunda-feira</Text>
-            </IntervalDay>
-            <IntervalInputs>
-              <TextInput
-                size={'sm'}
-                type="time"
-                step={60}
-                crossOrigin="anonymous"
-              />
-              <TextInput
-                size={'sm'}
-                type="time"
-                step={60}
-                crossOrigin="anonymous"
-              />
-            </IntervalInputs>
-          </IntervalItem>
-          <IntervalItem>
-            <IntervalDay>
-              <Checkbox />
-              <Text>Terça-feira</Text>
-            </IntervalDay>
-            <IntervalInputs>
-              <TextInput
-                size={'sm'}
-                type="time"
-                step={60}
-                crossOrigin="anonymous"
-              />
-              <TextInput
-                size={'sm'}
-                type="time"
-                step={60}
-                crossOrigin="anonymous"
-              />
-            </IntervalInputs>
-          </IntervalItem>
+          {fields.map((fields, index) => {
+            return (
+              <IntervalItem key={fields.id}>
+                <IntervalDay>
+                  <Checkbox />
+                  <Text>{weekDays[fields.weekday]}</Text>
+                </IntervalDay>
+                <IntervalInputs>
+                  <TextInput
+                    size={'sm'}
+                    type="time"
+                    step={60}
+                    crossOrigin="anonymous"
+                    {...register(`intervals.${index}.starTime`)}
+                  />
+                  <TextInput
+                    size={'sm'}
+                    type="time"
+                    step={60}
+                    crossOrigin="anonymous"
+                    {...register(`intervals.${index}.endTime`)}
+                  />
+                </IntervalInputs>
+              </IntervalItem>
+            )
+          })}
         </IntervalContainer>
         <Button type="submit">
           Próximo passo <ArrowRight />
