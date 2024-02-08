b2d7330910077c5220b08392f317cc9902e3260a
pedroluc-cpu <pedrolucas_gta2015@hotmail.com>
2024-02-07 21:55:44 -0300

    fix: Configurando disponibilidade - Página de disponibilidade

---
+++ src/pages/register/time-intervals/styles.ts
@@ -0,0 +1,41 @@
+import { Box, styled } from '@ignite-ui/react'
+
+export const IntervalBox = styled(Box, {
+  marginTop: '$6',
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+export const IntervalContainer = styled('div', {
+  border: '1px solid $gray600',
+  borderRadius: '$md',
+  marginBottom: '$4',
+})
+
+export const IntervalItem = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  padding: '$3 $4',
+
+  '& + &': {
+    border: '1px solid $gray600',
+  },
+})
+
+export const IntervalDay = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$3',
+})
+
+export const IntervalInputs = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$3',
+
+  'input::-webkit-calendar-picker-indicator': {
+    filter: 'invert(100%) brightness(30%)',
+    cursor: 'pointer',
+  },
+})
