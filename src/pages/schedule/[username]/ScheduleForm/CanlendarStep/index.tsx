f3c2406277dfedf1d6d2e77bbb365dd0fcb5d435
pedroluc-cpu <pedrolucas_gta2015@hotmail.com>
2024-02-13 16:43:35 -0300

    fix: Calendário e disponibilidade - Tabela de agendamentos

--- src/pages/schedule/[username]/ScheduleForm/CanlendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CanlendarStep/index.tsx
@@ -2,18 +2,24 @@ import { Calendar } from '@/components/Canlendar'
 import { Container } from './styles'
 import * as TimePicker from './styles'
 import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
 
   const isDateSelected = !!selectedDate
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
   return (
     <Container isTimePickerOpen={isDateSelected}>
       <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
       {isDateSelected && (
         <TimePicker.TimePickerRoot>
           <TimePicker.TimePickerHeader>
-            Terça-feira <span>20 de setembro</span>
+            {weekDay} <span>{describedDate}</span>
           </TimePicker.TimePickerHeader>
           <TimePicker.TimePickerList>
             <TimePicker.TimePickerItem>08:00h</TimePicker.TimePickerItem>
